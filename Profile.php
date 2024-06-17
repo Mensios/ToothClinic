412f0663f42dd09a3dec916d4da401033537bc85
Mensios <113691327+Mensios@users.noreply.github.com>
2024-06-17 16:53:38 +0800

    Delete Profile.php

--- Profile.php
+++
@@ -1,8 +0,0 @@
-<?php
-sesssion_start();
-include ("../../connect_db.php");
-$id = $_SESSION['id'];
-
-$query = "SELECT * FROM Users WHERE id_user='$id'";
-$res = mysqli_query($link, $query);
-$user = mysqli_fetch_assoc()
